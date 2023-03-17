d87cb035c1aa68de67e6e49ceceabffb45f9db2f
Koji Kawamura <ijokarumawak@gmail.com>
2023-03-17 17:59:24 +0900

    Fix lint errors.

--- src/components/obsMediaStatus.tsx
+++ src/components/obsMediaStatus.tsx
@@ -1,89 +1,92 @@
 import React, { useState, useEffect } from 'react'
-import { css } from '@mui/styled-engine'
 import OBSWebSocket from 'obs-websocket-js'
 
 type Props = {
-  obsUrl?: string,
+  obsUrl?: string
   obsPassword?: string
 }
 
-export const ObsMediaStatus: React.FC<Props> = ({obsUrl, obsPassword}) => {
-
+export const ObsMediaStatus: React.FC<Props> = ({ obsUrl, obsPassword }) => {
   const [isObsMonitoring, setIsObsMonitoring] = useState(false)
   // TODO: get current media input name using API
-  const [mediaInputName, setMediaInputName] = useState({inputName: ''})
-  const [mediaStatusInterval, setMediaStatusInterval] = useState()
+  const [mediaInputName, setMediaInputName] = useState({ inputName: '' })
   const [mediaStatusText, setMediaStatusText] = useState('')
   const [mediaStatusProgress, setMediaStatusProgress] = useState(0)
 
-
-  const progressBarStyle = {backgroundColor: '#dddddd', width: mediaStatusProgress  + '%'}
+  const progressBarStyle = {
+    backgroundColor: '#dddddd',
+    width: mediaStatusProgress + '%',
+  }
 
   useEffect(() => {
-    console.log(
-      "Occurs ONCE, AFTER the initial render.", isObsMonitoring
-    )
+    console.log('Occurs ONCE, AFTER the initial render.', isObsMonitoring)
 
-    const obs = new OBSWebSocket();
-    obs.on('ConnectionError', err => {
+    const obs = new OBSWebSocket()
+    obs.on('ConnectionError', (err) => {
       console.error('Socket error:', err)
     })
-    
-    obs.on('MediaInputPlaybackStarted', async inputName => {
+
+    obs.on('MediaInputPlaybackStarted', async (inputName) => {
       console.log('MediaInputPlaybackStarted', inputName)
       setMediaInputName(inputName)
     })
-    
-    obs.on('MediaInputActionTriggered', data => {
+
+    obs.on('MediaInputActionTriggered', (data) => {
       console.log('MediaInputActionTriggered', data)
     })
-    
-    obs.on('MediaInputPlaybackEnded', async inputName => {
+
+    obs.on('MediaInputPlaybackEnded', async (inputName) => {
       console.log('MediaInputPlaybackEnded', inputName)
-      setMediaInputName({inputName: ''})
+      setMediaInputName({ inputName: '' })
     })
-    
-    
+
     async function connect() {
       await disconnect()
       try {
         console.log('Connecting to:', obsUrl, '- using password:', obsPassword)
-        const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(obsUrl, obsPassword)
-        console.log(`Connected to obs-websocket version ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`)
-      } catch (e:any) {
+        const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
+          obsUrl,
+          obsPassword,
+        )
+        console.log(
+          `Connected to obs-websocket version ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`,
+        )
+      } catch (e) {
         console.log(e)
       }
     }
-    
+
     async function disconnect() {
       await obs.disconnect()
     }
-    
+
     async function sendCommand(command: any, params: any) {
       try {
         return await obs.call(command, params || {})
-      } catch (e) {
+      } catch (e: any) {
         console.log('Error sending command', command, ' - error is:', e.message)
         return {}
       }
     }
-    
+
     function secToStr(n: number) {
-      const min = Math.round(n / 60);
-      const sec = Math.round(n % 60);
-  
-      return String(min).padStart(2, '0') + ':' + String(sec).padStart(2, '0');
+      const min = Math.round(n / 60)
+      const sec = Math.round(n % 60)
+
+      return String(min).padStart(2, '0') + ':' + String(sec).padStart(2, '0')
     }
-  
+
     async function startObsMonitor() {
       setIsObsMonitoring(true)
       await connect()
-  
-      // TODO: Somehow, something updates the mediaInputName. This code should be moved at the beginning to set the initial 
-      const inputList = await sendCommand('GetInputList', {inputKind: 'vlc_source'})
+
+      // TODO: Somehow, something updates the mediaInputName. This code should be moved at the beginning to set the initial
+      const inputList = await sendCommand('GetInputList', {
+        inputKind: 'vlc_source',
+      })
       if (inputList && inputList['inputs']) {
-        inputList['inputs'].forEach(async (element:any) => {
-          const inputName = {inputName: element['inputName']}
+        inputList['inputs'].forEach(async (element: any) => {
+          const inputName = { inputName: element['inputName'] }
           const media = await sendCommand('GetMediaInputStatus', inputName)
           if (media['mediaState'] === 'OBS_MEDIA_STATE_PLAYING') {
             console.log('Found active media', media)
@@ -92,31 +95,35 @@ export const ObsMediaStatus: React.FC<Props> = ({obsUrl, obsPassword}) => {
         })
       }
 
-      let interval: any = setInterval(async () => {
-        console.log(mediaInputName['inputName'].length);
+      setInterval(async () => {
+        console.log(mediaInputName['inputName'].length)
         if (mediaInputName['inputName'].length == 0) {
-          return;
+          return
         }
-  
+
         const media = await sendCommand('GetMediaInputStatus', mediaInputName)
         if (!media['mediaDuration']) {
-          return;
+          return
         }
-        console.log('mediastatus', media);
+        console.log('mediastatus', media)
         const remaining = (media['mediaDuration'] - media['mediaCursor']) / 1000
-        
-        setMediaStatusProgress(media['mediaCursor'] / media['mediaDuration'] * 100)
-        setMediaStatusText('total: ' + secToStr(media['mediaDuration'] / 1000)
-          + ' passed: ' + secToStr(media['mediaCursor'] / 1000)
-          + ' remaining: ' + secToStr(remaining))
-  
+
+        setMediaStatusProgress(
+          (media['mediaCursor'] / media['mediaDuration']) * 100,
+        )
+        setMediaStatusText(
+          'total: ' +
+            secToStr(media['mediaDuration'] / 1000) +
+            ' passed: ' +
+            secToStr(media['mediaCursor'] / 1000) +
+            ' remaining: ' +
+            secToStr(remaining),
+        )
       }, 1000)
-      setMediaStatusInterval(interval)
-  
     }
 
     startObsMonitor()
-  }, []);
+  }, [])
 
   return (
     <div>
