b7edd67d31c24d0da505d1987f3932f0c2b4fa8f
Koji Kawamura <ijokarumawak@gmail.com>
2023-03-20 16:41:54 +0900

    Use obs-monitor-api running on the elk server.

--- src/components/obsMediaStatus.tsx
+++ src/components/obsMediaStatus.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import OBSWebSocket from 'obs-websocket-js'
 
 type Props = {
   obsUrl?: string
@@ -7,68 +6,16 @@ type Props = {
 }
 
 export const ObsMediaStatus: React.FC<Props> = ({ obsUrl, obsPassword }) => {
-  const [isObsMonitoring, setIsObsMonitoring] = useState(false)
-  // TODO: get current media input name using API
-  const [mediaInputName, setMediaInputName] = useState({ inputName: '' })
+  const [mediaInputName, setMediaInputName] = useState('')
   const [mediaStatusText, setMediaStatusText] = useState('')
   const [mediaStatusProgress, setMediaStatusProgress] = useState(0)
 
   const progressBarStyle = {
-    backgroundColor: '#dddddd',
+    backgroundColor: '#337868',
     width: mediaStatusProgress + '%',
   }
 
   useEffect(() => {
-    console.log('Occurs ONCE, AFTER the initial render.', isObsMonitoring)
-
-    const obs = new OBSWebSocket()
-    obs.on('ConnectionError', (err) => {
-      console.error('Socket error:', err)
-    })
-
-    obs.on('MediaInputPlaybackStarted', async (inputName) => {
-      console.log('MediaInputPlaybackStarted', inputName)
-      setMediaInputName(inputName)
-    })
-
-    obs.on('MediaInputActionTriggered', (data) => {
-      console.log('MediaInputActionTriggered', data)
-    })
-
-    obs.on('MediaInputPlaybackEnded', async (inputName) => {
-      console.log('MediaInputPlaybackEnded', inputName)
-      setMediaInputName({ inputName: '' })
-    })
-
-    async function connect() {
-      await disconnect()
-      try {
-        console.log('Connecting to:', obsUrl, '- using password:', obsPassword)
-        const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
-          obsUrl,
-          obsPassword,
-        )
-        console.log(
-          `Connected to obs-websocket version ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`,
-        )
-      } catch (e) {
-        console.log(e)
-      }
-    }
-
-    async function disconnect() {
-      await obs.disconnect()
-    }
-
-    async function sendCommand(command: any, params: any) {
-      try {
-        return await obs.call(command, params || {})
-      } catch (e: any) {
-        console.log('Error sending command', command, ' - error is:', e.message)
-        return {}
-      }
-    }
-
     function secToStr(n: number) {
       const min = Math.round(n / 60)
       const sec = Math.round(n % 60)
@@ -76,58 +23,47 @@ export const ObsMediaStatus: React.FC<Props> = ({ obsUrl, obsPassword }) => {
       return String(min).padStart(2, '0') + ':' + String(sec).padStart(2, '0')
     }
 
-    async function startObsMonitor() {
-      setIsObsMonitoring(true)
-      await connect()
-
-      // TODO: Somehow, something updates the mediaInputName. This code should be moved at the beginning to set the initial
-      const inputList = await sendCommand('GetInputList', {
-        inputKind: 'vlc_source',
-      })
-      if (inputList && inputList['inputs']) {
-        inputList['inputs'].forEach(async (element: any) => {
-          const inputName = { inputName: element['inputName'] }
-          const media = await sendCommand('GetMediaInputStatus', inputName)
-          if (media['mediaState'] === 'OBS_MEDIA_STATE_PLAYING') {
-            console.log('Found active media', media)
-            setMediaInputName(inputName)
-          }
-        })
+    const interval = setInterval(async () => {
+      if (obsUrl === undefined || obsUrl === null || obsUrl.length === 0) {
+        return
       }
+      fetch(obsUrl)
+        .then((response) => response.json())
+        .then((media) => {
+          console.log(media)
 
-      setInterval(async () => {
-        console.log('showing length ', mediaInputName['inputName'].length)
-        if (mediaInputName['inputName'].length == 0) {
-          return
-        }
+          if (!media['mediaDuration']) {
+            setMediaInputName('')
+            setMediaStatusText('')
+            setMediaStatusProgress(0)
+            return
+          }
+          console.log('media', media)
 
-        const media = await sendCommand('GetMediaInputStatus', mediaInputName)
-        if (!media['mediaDuration']) {
-          return
-        }
-        console.log('mediastatus', media)
-        const remaining = (media['mediaDuration'] - media['mediaCursor']) / 1000
+          setMediaInputName(media['inputName'])
 
-        setMediaStatusProgress(
-          (media['mediaCursor'] / media['mediaDuration']) * 100,
-        )
-        setMediaStatusText(
-          'total: ' +
-            secToStr(media['mediaDuration'] / 1000) +
-            ' passed: ' +
-            secToStr(media['mediaCursor'] / 1000) +
-            ' remaining: ' +
-            secToStr(remaining),
-        )
-      }, 1000)
-    }
+          const remaining =
+            (media['mediaDuration'] - media['mediaCursor']) / 1000
 
-    startObsMonitor()
-  }, [])
+          setMediaStatusProgress(
+            (media['mediaCursor'] / media['mediaDuration']) * 100,
+          )
+          setMediaStatusText(
+            'total: ' +
+              secToStr(media['mediaDuration'] / 1000) +
+              ' passed: ' +
+              secToStr(media['mediaCursor'] / 1000) +
+              ' remaining: ' +
+              secToStr(remaining),
+          )
+        })
+    }, 1000)
+    return () => clearInterval(interval)
+  })
 
   return (
     <div>
-      <div>{mediaInputName['inputName']}</div>
+      <div>{mediaInputName}</div>
       <div>{mediaStatusText}</div>
       <div css={progressBarStyle}>&nbsp;</div>
     </div>
--- src/data.ts
+++ src/data.ts
@@ -3,6 +3,8 @@ type Data = {
   name: string
   videoPlatform: string
   videoId: string
+  obsUrl?: string
+  obsPassword?: string
 }
 
 export const data: Array<Data> = [
@@ -11,20 +13,23 @@ export const data: Array<Data> = [
     name: 'A',
     videoPlatform: 'ivs',
     videoId:
-      'https://d3pun3ptcv21q4.cloudfront.net/medialive/o11y2022/talks/1381/1381.m3u8',
+      'https://294fdb0f5e90.us-east-1.playback.live-video.net/api/video/v1/us-east-1.607167088920.channel.LJst1Bb2o3eq.m3u8',
+    obsUrl: 'https://elk.cloudnativedays.jp/obs-monitor-api/media-progress/A',
   },
   {
     id: 30,
     name: 'B',
     videoPlatform: 'ivs',
     videoId:
-      'https://d3pun3ptcv21q4.cloudfront.net/medialive/o11y2022/talks/1374/1374.m3u8',
+      'https://294fdb0f5e90.us-east-1.playback.live-video.net/api/video/v1/us-east-1.607167088920.channel.g9ZfIZVvPmVb.m3u8',
+    obsUrl: 'https://elk.cloudnativedays.jp/obs-monitor-api/media-progress/B',
   },
   {
     id: 31,
     name: 'C',
     videoPlatform: 'ivs',
     videoId:
-      'https://d3pun3ptcv21q4.cloudfront.net/medialive/o11y2022/talks/1355/1355.m3u8',
+      'https://294fdb0f5e90.us-east-1.playback.live-video.net/api/video/v1/us-east-1.607167088920.channel.8UjYSfSj653l.m3u8',
+    obsUrl: 'https://elk.cloudnativedays.jp/obs-monitor-api/media-progress/C',
   },
 ]
